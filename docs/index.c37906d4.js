44895f42668a915bf47c775d1e02b1b490047902
Un4givenPG <26605596+Un4givenPG@users.noreply.github.com>
2022-02-20 13:37:00 +0100

    Conic gradient

---
+++ docs/index.c37906d4.js
@@ -0,0 +1,2 @@
+function deglitcher(){var t=document.getElementById("bottom-gl"),e=document.getElementById("upper-gl");setTimeout((function(){t.classList.add("glitch-off"),t.classList.remove("glitch"),e.classList.add("glitch-off"),e.classList.remove("glitch")}),3e3)}deglitcher();
+//# sourceMappingURL=index.c37906d4.js.map
--- src/js/glitch.js
+++ src/js/glitch.js
@@ -3,13 +3,10 @@ deglitcher();
 function deglitcher() {
     const glitch = document.getElementById('bottom-gl');
     const glitch2 = document.getElementById('upper-gl');
-    console.log(glitch)
-    console.log(glitch2)
     setTimeout(function () {
         glitch.classList.add('glitch-off');
         glitch.classList.remove('glitch'); 
         glitch2.classList.add('glitch-off');
         glitch2.classList.remove('glitch'); 
-        console.log('Trele Morele');
     }, 3000);
 }
