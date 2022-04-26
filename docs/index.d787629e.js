46b5719e4db42ab91c17668072f5adda37a78e97
Un4givenPG <26605596+Un4givenPG@users.noreply.github.com>
2022-04-27 00:46:08 +0200

    Project sanitazation

--- docs/index.d787629e.js
+++
@@ -1,2 +0,0 @@
-function deglitcher(){var e=document.getElementById("bottom-gl"),l=document.getElementById("upper-gl");console.log(e),console.log(l),setTimeout((function(){e.classList.add("glitch-off"),e.classList.remove("glitch"),l.classList.add("glitch-off"),l.classList.remove("glitch"),console.log("Trele Morele")}),5e3)}deglitcher();
-//# sourceMappingURL=index.d787629e.js.map
--- docs/index.ff195d7c.js
+++
@@ -1,2 +0,0 @@
-function deglitcher(){var t=document.getElementById("bottom-gl"),e=document.getElementById("upper-gl");setTimeout((function(){t.classList.add("glitch-off"),t.classList.remove("glitch"),e.classList.add("glitch-off"),e.classList.remove("glitch")}),3e3)}deglitcher();
-//# sourceMappingURL=index.ff195d7c.js.map
